5c3ee0073eaa49c76f178403ce9ffc935b2571fc
akosiJ <akosij@J>
2024-02-29 15:53:47 +0800

    [added] landing-page component [added] timelog service [added] webcam capability without ussage yet [added] manual time log in form
    time log fully working functions
    - time in
    - time out
    - check if input email exist
    - check if input email is valid
    - snackbar on confimation of time log
    - enter key reacts to time

---
+++ src/app/db/pocketbase-timelog.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import PocketBase from 'pocketbase';
+import { environment } from '../environment/environment.development';
+const pb = new PocketBase(environment.pocketbase.url);
+@Injectable({
+  providedIn: 'root',
+})
+export class PocketbaseTimelogService {
+  constructor() {}
+  createTimeLog = async (email: string | null) => {
+    let dateToday = new Date();
+    let data = {
+      timeIn: dateToday,
+      employeeEmailAddress: email,
+    };
+    // check if time-in already exist, getFirstItem always return value [?]
+    return await pb
+      .collection('time_logs')
+      .getFirstListItem(
+        `timeIn >= '${dateToday
+          .toISOString()
+          .slice(0, 10)}' && employeeEmailAddress = '${email}'`
+      )
+      .then(async (res) => {
+        if (res) {
+          // time-in already exist procees to time-out update record
+          return await pb
+            .collection('time_logs')
+            .update(`${res.id}`, {
+              timeOut: dateToday,
+              employeeEmailAddress: email,
+            })
+            .then((res) => {
+              // time out update record success
+              return { ...res, success: 'timeOut' };
+            })
+            .catch((error) => {
+              // update record failed
+              throw error;
+            });
+        }
+        return res;
+      })
+      .catch(async (error) => {
+        if (error.data.code == 404) {
+          // create new time log
+          return await pb
+            .collection('time_logs')
+            .create(data)
+            .then((res) => {
+              return { ...res, success: 'timeIn' };
+            })
+            .catch((error) => {
+              // email input did not exist
+              throw error;
+            });
+        } else {
+          return;
+        }
+      });
+  };
+}
